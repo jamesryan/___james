542433d70f0771c355a739b3ab52c9152c3eb38a
jamesrs <jamesrs@gmail.com>
2015-10-16 07:34:00 -0600

    site files

--- gulpfile.js
+++ gulpfile.js
@@ -21,23 +21,6 @@ gulp.task('scripts', function () {
        .pipe(gulp.dest('_/js'))
 });
 
-gulp.task('vendors', function () {
-  return gulp.src('dev/vendor/*.js')
-          .pipe(uglify())
-          .pipe(concat('vendors.js'))
-          .pipe(gulp.dest('js/vendor'))
-  });
-
-gulp.task('jadei', function () {
-  return gulp.src('*.jade')
-        .pipe(jade())
-        .pipe(gulp.dest(''));
-  });
-gulp.task('jadeT', function () {
-  return gulp.src('dev/templates/**/*.jade')
-        .pipe(jade())
-        .pipe(gulp.dest('pub_pages'));
-  });
 
 //watcher task
 gulp.task('watch', function() {
@@ -45,13 +28,7 @@ gulp.task('watch', function() {
   gulp.watch('dev/scss/*.scss', ['sass']);
    // Watch .js files
   gulp.watch('dev/js/**/*.js', ['scripts']);
-
-  gulp.watch('dev/vendor/*.js', ['vendors']);
-   // Watch index files
-  gulp.watch('*.jade', ['jadei']);
-  // Watch template files
-  gulp.watch('dev/templates/**/*.jade', ['jadeT']);
  });
 
 // Default tasks
-gulp.task('default', ['sass', 'scripts', 'vendors', 'jadei', 'jadeT']);
+gulp.task('default', ['sass', 'scripts']);
